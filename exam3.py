82fa1842e55aa652787f9812be124ffe3deb150c
lukas <lukas.turskas@codeacademylt.onmymicrosoft.com>
2024-05-23 20:38:26 +0300

    testing

--- exam3.py
+++ exam3.py
@@ -4,3 +4,22 @@
 # - color: A tuple with three integers representing the RGB values of the color (each value ranges from 0 to 255).
 # Returns:
 # - A tuple representing the inverted RGB values.
+
+
+def color_invert(color):
+    inverted_color = tuple(255 - value for value in color)
+    return inverted_color
+color = input('Input color by words or type done: ').lower()
+if color == 'done' :
+    red = int(input('input the red value(0-255): '))
+    green = int(input('input the green value(0-255): '))
+    blue = int(input('input the blue value (0-255): '))
+    color = (red, green, blue)
+    inverted_color = color_invert(color)
+    print(f'Inverted RGB color is: {inverted_color}')
+else:
+    print('wrong information, abort!')
+
+
+
+
